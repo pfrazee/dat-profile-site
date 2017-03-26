8179d58f5b265775230fa8718ec20cdbdc2baaa1
Paul Frazee <pfrazee@gmail.com>
2017-03-25 20:18:07 -0500

    Replace bad extensible-error dep

--- errors.js
+++ errors.js
@@ -1,6 +1,17 @@
-const ExtensibleError = require('extensible-error')
+class ExtendableError extends Error {
+  constructor(msg) {
+    super(msg)
+    this.name = this.constructor.name
+    this.message = msg
+    if (typeof Error.captureStackTrace === 'function') {
+      Error.captureStackTrace(this, this.constructor)
+    } else { 
+      this.stack = (new Error(msg)).stack 
+    }
+  }
+}
 
-exports.MissingParameterError = class MissingParameterError extends ExtensibleError {
+exports.MissingParameterError = class MissingParameterError extends ExtendableError {
   constructor (msg) {
     super(msg || 'Missing a required parameter')
     this.missingParameter = true
